f96a19c8898d7b6b743956b9b07902763b4ee38d
Roger Chan <roger.chan@cargosmart.com>
2015-12-14 09:36:41 +0800

    Push change

--- src/main/java/hello/OAuth2ServerConfiguration.java
+++ src/main/java/hello/OAuth2ServerConfiguration.java
@@ -18,10 +18,12 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -30,88 +32,115 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class OAuth2ServerConfiguration {
 
-	private static final String RESOURCE_ID = "restservice";
+    private static final String RESOURCE_ID = "restservice";
 
-	@Configuration
-	@EnableResourceServer
-	protected static class ResourceServerConfiguration extends
-			ResourceServerConfigurerAdapter {
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends
+            ResourceServerConfigurerAdapter {
 
-		@Override
-		public void configure(ResourceServerSecurityConfigurer resources) {
-			// @formatter:off
-			resources
-				.resourceId(RESOURCE_ID);
-			// @formatter:on
-		}
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) {
+            // @formatter:off
+            resources
+                    .resourceId(RESOURCE_ID);
+            // @formatter:on
+        }
 
-		@Override
-		public void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http
-				.authorizeRequests()
-					.antMatchers("/users").hasRole("ADMIN")
-					.antMatchers("/greeting").authenticated();
-			// @formatter:on
-		}
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    .authorizeRequests()
+                    .antMatchers("/users").hasRole("ADMIN")
+                    .antMatchers("/greeting").authenticated();
+            // @formatter:on
+        }
 
-	}
+    }
 
-	@Configuration
-	@EnableAuthorizationServer
-	protected static class AuthorizationServerConfiguration extends
-			AuthorizationServerConfigurerAdapter {
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfiguration extends
+            AuthorizationServerConfigurerAdapter {
 
-		private TokenStore tokenStore = new InMemoryTokenStore();
 
-		@Autowired
-		@Qualifier("authenticationManagerBean")
-		private AuthenticationManager authenticationManager;
+        @Autowired
+        private DataSource dataSource;
 
-		@Autowired
-		private CustomUserDetailsService userDetailsService;
+        //private TokenStore tokenStore = new InMemoryTokenStore();
 
-		@Override
-		public void configure(AuthorizationServerEndpointsConfigurer endpoints)
-				throws Exception {
-			// @formatter:off
-			endpoints
-				.tokenStore(this.tokenStore)
-				.authenticationManager(this.authenticationManager)
-				.userDetailsService(userDetailsService);
-			// @formatter:on
-		}
+        @Autowired
+        @Qualifier("authenticationManagerBean")
+        private AuthenticationManager authenticationManager;
 
-		@Override
-		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-			// @formatter:off
-			clients
-				.inMemory()
-					.withClient("clientapp")
-						.authorizedGrantTypes("password", "refresh_token")
-						.authorities("USER")
-						.scopes("read", "write")
-						.resourceIds(RESOURCE_ID)
-						.secret("123456");
-			// @formatter:on
-		}
+        @Autowired
+        private CustomUserDetailsService userDetailsService;
 
-		@Bean
-		@Primary
-		public DefaultTokenServices tokenServices() {
-			DefaultTokenServices tokenServices = new DefaultTokenServices();
-			tokenServices.setSupportRefreshToken(true);
-			tokenServices.setTokenStore(this.tokenStore);
-			return tokenServices;
-		}
 
-	}
+        @Bean
+        public JdbcTokenStore tokenStore() {
+            return new JdbcTokenStore(dataSource);
+        }
+
+//        @Bean
+//        JdbcApprovalStore jdbcApprovalStore(){
+//            return new JdbcApprovalStore(dataSource);
+//        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+                throws Exception {
+            // @formatter:off
+            endpoints
+                    .tokenStore(tokenStore())
+                    .authenticationManager(this.authenticationManager)
+                    //.approvalStore(jdbcApprovalStore())
+                    .userDetailsService(userDetailsService);
+            // @formatter:on
+        }
+
+
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            // @formatter:off
+//            DataSource dataSource = DataSourceBuilder.create()
+//                    .driverClassName("com.mysql.jdbc.Driver")
+//                    .url("jdbc:mysql://rtest08.oocl.com:43306/learning").username("root").password("password").build();
+
+            clients
+                    .jdbc(dataSource);
+            //.inMemory()
+//            .withClient("clientapp")
+//            .authorizedGrantTypes("password", "refresh_token")
+//            .authorities("USER")
+//            .scopes("read", "write")
+//            .resourceIds(RESOURCE_ID)
+//            .secret("123456");
+            // @formatter:on
+        }
+
+        @Bean
+        @Primary
+        public DefaultTokenServices tokenServices() {
+            DefaultTokenServices tokenServices = new DefaultTokenServices();
+            tokenServices.setSupportRefreshToken(true);
+            tokenServices.setTokenStore(tokenStore());
+            return tokenServices;
+        }
+
+    }
 
 }
